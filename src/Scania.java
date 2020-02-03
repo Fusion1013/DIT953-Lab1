897e64def4eeb1e8da4798a53e56ccdf8d8aabe9
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2020-02-03 14:04:06 +0100

    Added new Class Scania

---
+++ src/Scania.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Scania extends Car {
+    /***
+     * Creates a new scania truck
+     * @param nrDoors the number of doors of the car
+     * @param color the color of the car
+     * @param enginePower the cars engine power
+     * @param modelName the model name of the car
+     */
+    public Scania(int nrDoors, Color color, double enginePower, String modelName) {
+        super(2, Color.green, 20, "ScaniaVabis314");
+    }
+}
