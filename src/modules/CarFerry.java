1d0a299b7db9a693e2c99f980461f3a2f5156204
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2020-02-07 14:03:31 +0100

    Implemented Ferry

--- src/modules/CarFerry.java
+++ src/modules/CarFerry.java
@@ -1,16 +1,49 @@
 package modules;
 
-import interfaces.IContainer;
+import interfaces.IMovable;
+import parents.Container;
+import parents.Movable;
 
-public class CarFerry implements IContainer<Car> {
+public class CarFerry<T extends Car> implements IMovable {
 
-    @Override
-    public void Load(Car loadable) {
+    Container<T> parentContainer;
+    Movable parentMovable;
 
+    public CarFerry(int capacity){
+        parentContainer = new Container(capacity);
+        parentMovable = new Movable();
+    }
+
+    public void Load(T car) {
+        parentContainer.Load(car);
+    }
+
+    public T Unload() {
+        return parentContainer.Unload(0);
     }
 
     @Override
-    public Car Unload(int pos) {
-        return null;
+    public void move() {
+        parentMovable.move();
+    }
+
+    @Override
+    public void turnLeft() {
+        parentMovable.turnLeft();
+    }
+
+    @Override
+    public void turnRight() {
+        parentMovable.turnRight();
+    }
+
+    @Override
+    public void incrementSpeed(double amount) {
+        parentMovable.incrementSpeed(amount);
+    }
+
+    @Override
+    public void decrementSpeed(double amount) {
+        parentMovable.decrementSpeed(amount);
     }
 }
