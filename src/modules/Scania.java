f961967bf1b573d78b46ed9ad5bb5e286f3b6a4c
martinbcd <martinbcd@live.se>
2020-02-07 14:31:46 +0100

    modified comments on Workshop volvo vector2 scania

--- src/modules/Scania.java
+++ src/modules/Scania.java
@@ -3,7 +3,6 @@ package modules;
 import java.awt.*;
 
 public class Scania extends Car {
-
     private double truckbedAngle = 0;
 
     /***
@@ -13,16 +12,29 @@ public class Scania extends Car {
         super(2, Color.green, 20, "ScaniaVabis314");
     }
 
+    /***
+     * increases the angle of the truckbed by the specified amount
+     * @param amount the double amount to increase the truckbed
+     */
     public void IncreaseAngle(double amount){
         truckbedAngle += amount;
         NormalizeAngle();
     }
+    /***
+     * decreases the angle of the truckbed by the specified amount
+     * @param amount the double amount to decrease the truckbed
+     */
 
     public void DecreaseAngle(double amount){
         truckbedAngle -= amount;
         NormalizeAngle();
     }
 
+    /***
+     * a sanity check designed to prevent the trucks truckbed to not reach absurd angles
+     * keeping it between 0 and 70
+     * also if an angle would try be changed while driving the angle will not change
+     */
     private void NormalizeAngle(){
         truckbedAngle = Math.min(truckbedAngle, 70);
         truckbedAngle = Math.max(truckbedAngle, 0);
@@ -30,9 +42,17 @@ public class Scania extends Car {
         if (getCurrentSpeed() != 0) truckbedAngle = 0;
     }
 
+    /***
+     *
+     * @return returns the angle of the truckbed
+     */
     public double getAngle(){
         return truckbedAngle;
     }
+
+    /***
+     * starts the engine
+     */
 
     @Override
     public void startEngine(){
@@ -40,6 +60,11 @@ public class Scania extends Car {
             super.startEngine();
         }
     }
+
+    /***
+     * increases the speed by the specified amount
+     * @param amount The amount to increase the current speed by
+     */
 
     @Override
     public void incrementSpeed(double amount){
