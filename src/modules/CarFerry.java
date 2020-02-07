ba4ec3ebe78d54b28a1cee88eb4f80618efd90d4
DragonPaint <ajdinkrkic9@gmail.com>
2020-02-07 13:55:00 +0100

    Herro

--- src/modules/CarFerry.java
+++ src/modules/CarFerry.java
@@ -4,6 +4,10 @@ import interfaces.IContainer;
 
 public class CarFerry implements IContainer<Car> {
 
+    /***
+     * Herro
+     * @param loadable
+     */
     @Override
     public void Load(Car loadable) {
 
